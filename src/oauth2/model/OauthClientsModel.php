a811387373eb2826e6d14ed1c752244f407a8222
hebidu <hebiduhebi@126.com>
2017-12-20 17:30:27 +0800

    【完善】OauthClients表前缀修改成Common

--- src/oauth2/model/OauthClientsModel.php
+++ src/oauth2/model/OauthClientsModel.php
@@ -13,7 +13,5 @@ use by\component\tp5\model\BaseModel;
 class OauthClientsModel extends BaseModel
 {
     // 设置当前模型对应的完整数据表名称
-    protected $table = 'oauth_clients';
-
-    protected $connection = "oauth_db";
+    protected $table = 'common_clients';
 }
